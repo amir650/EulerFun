97758cd7e2c4e2b3de95b2434656ad8d1fb22fd8
Amir Afghani <amir.afghani@gmail.com>
2014-06-27 16:10:59 -0700

    Solution to Problem 8

--- ProjectEuler/src/Problem8.scala
+++ ProjectEuler/src/Problem8.scala
@@ -2,6 +2,7 @@ object Problem8 {
 
   //Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.
   // What is the value of this product?
+  //
 
   def main(args: Array[String]) {
 
