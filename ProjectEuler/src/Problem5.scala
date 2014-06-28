7a5b54394645683c36daf360f61b3c0762636510
Amir Afghani <amir.afghani@gmail.com>
2014-06-27 17:02:46 -0700

    Solution to Problem 5

---
+++ ProjectEuler/src/Problem5.scala
@@ -0,0 +1,14 @@
+object Problem5 {
+
+  //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+  def main(args: Array[String]) {
+
+    System.out.println(Iterator.iterate(20L)(_+20L).find(isEvenlyDivisible))
+
+  }
+
+  def isEvenlyDivisible(n: Long) : Boolean = (1 to 20).forall(n % _ == 0)
+
+}
