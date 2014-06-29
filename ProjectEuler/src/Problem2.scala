b821db7e54f15168f2e6c4b8825134d523b1d0d4
Amir Afghani <amir.afghani@gmail.com>
2014-06-29 11:53:24 -0700

    Problem 2

--- ProjectEuler/src/Problem2.scala
+++ ProjectEuler/src/Problem2.scala
@@ -8,6 +8,7 @@ object Problem2 {
     // the first 10 terms will be:
     // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
     //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
     print(fibonacciNumbers().takeWhile(_ < 4000000).filter(isEven).sum)
 
   }
